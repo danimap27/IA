35cea8ce762912c429648ebda8fa2af45fc1ce0a
Daniel Martin <danimp2002@gmail.com>
2024-11-22 12:28:34 +0100

    EPD5, EPD4, Tema5 ejercicios y inicialización cheatsheet para el examen

--- EPD/EPD2/.ipynb_checkpoints/computeCost-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# computeCost computes cost for linear regression using theta as the parameter for linear regression to fit the data points in X and y
-def computeCost(X, y, theta):
-    # Initialize some useful values
-    # You need to return the following variables correctly
-    # ====================== YOUR CODE HERE ======================
-    # Instructions: Compute the cost of a particular choice of theta. You should set J to the cost.
-    return ((np.sum(np.power((np.dot(X,theta)-y),2), axis=0))/(2*len(y) ))
